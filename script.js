8a9517361434990ca7c4afdb398ce2f6646ab5f5
John Rabe <johnmrabe@gmail.com>
2023-06-07 03:52:00 +0800

    Add replay and streak functionality

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const possibleMoves = ["Rock", "Paper", "Scissors"];
+const moves = document.querySelectorAll(".moves button");
+const player = document.getElementById("playerMove");
+const computer = document.getElementById("computerMove");
+const playerTally = document.getElementById("playerScore");
+const computerTally = document.getElementById("computerScore");
+const result = document.getElementById("result");
+const container = document.querySelector(".container");
+const streak = document.getElementById("streak");
+
 let playerScore = 0,
-  computerScore = 0;
+  computerScore = 0,
+  haveWinner = false;
+playerStreak = 0;
 
 const winningCombinations = {
   Rock: "Scissors",
@@ -29,15 +40,9 @@ function playRound(playerMove, computerMove) {
   }
 }
 
-const moves = document.querySelectorAll(".moves button");
-const player = document.getElementById("playerMove");
-const computer = document.getElementById("computerMove");
-const playerTally = document.getElementById("playerScore");
-const computerTally = document.getElementById("computerScore");
-const result = document.getElementById("result");
-
 moves.forEach((button) => {
   button.addEventListener("click", () => {
+    if (haveWinner === true) return;
     let playerChoice = button.id;
     let computerChoice = getComputerChoice();
 
@@ -46,7 +51,67 @@ moves.forEach((button) => {
     player.src = emojify[playerChoice];
     computer.src = emojify[computerChoice];
 
-    playerTally.innerText = playerScore;
-    computerTally.innerText = computerScore;
+    updateScore();
+    checkScore();
   });
 });
+
+function checkScore() {
+  if (playerScore < 5 && computerScore < 5) {
+    return;
+  } else if (playerScore === 5) {
+    result.innerText = "PLAYER WINS!";
+    playerStreak++;
+  } else {
+    result.innerText = "COMPUTER WINS!";
+    playerStreak = 0;
+  }
+  haveWinner = true;
+  streak.innerText = playerStreak;
+  drawPlayAgain();
+
+  moves.forEach((button) => {
+    button.classList.toggle("disabled");
+  });
+}
+
+function drawPlayAgain() {
+  // Remove replay button if it already exists
+  const replayButton = document.getElementById("replay");
+  if (replayButton) {
+    replayButton.remove();
+  }
+
+  const replay = document.createElement("button");
+  replay.setAttribute("id", "replay");
+  replay.setAttribute("class", "slide-down");
+  replay.textContent = "PLAY AGAIN";
+  replay.style.width = "360px";
+
+  container.appendChild(replay);
+
+  replay.addEventListener("click", () => {
+    replay.classList.remove("slide-up");
+    replay.offsetWidth;
+    replay.classList.add("slide-up");
+    haveWinner = false;
+    playerScore = 0;
+    computerScore = 0;
+
+    updateScore();
+
+    player.src = "./img/Rock.svg";
+    computer.src = "./img/Rock.svg";
+
+    result.innerText = "REMATCH!";
+
+    moves.forEach((button) => {
+      button.classList.toggle("disabled");
+    });
+  });
+}
+
+function updateScore() {
+  playerTally.innerText = playerScore;
+  computerTally.innerText = computerScore;
+}
